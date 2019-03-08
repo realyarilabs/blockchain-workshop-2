3bef90d8c6a5173b653071f00d53b6e81397233c
Helder Pinto <hp_ctt@hotmail.com>
2019-03-08 11:30:57 +0000

    wip 3

--- pet_shop_dapp/test/TestAdoption.js
+++ pet_shop_dapp/test/TestAdoption.js
@@ -11,23 +11,25 @@ contract("MetaCoin", ([adopterA, adopterB, adopterC, ...accounts ]) => {
     //The expected owner of adopted pet
     let expectedAdopter = adopterA;
 
-    before("init instance", async () => {
+    it("Init instance", async () => {
         adoptionInstance = await Adoption.deployed();
     })
 
-    it("Testing the adopt() function", () => {
-        const returnedId = adoptionInstance.adopt(expectedPetId);
-        assert.equal(returnedId, expectedPetId, "Adoption of the expected pet should match what is returned.");
+    it("Testing the adopt() function", async () => {
+        const returnedId = await adoptionInstance.adopt(expectedPetId, {
+            from: expectedAdopter
+        });
+        assert.ok(returnedId, "Adoption of the expected pet should match what is returned.");
     });
 
-    it("Testing retrieval of a single pet's owner", () => {
-        const adopterAddress = adoptionInstance.adopters(expectedPetId);
+    it("Testing retrieval of a single pet's owner", async () => {
+        const adopterAddress = await adoptionInstance.adopters(expectedPetId);
         assert.equal(adopterAddress, expectedAdopter, "Owner of the expected pet should be this contract");
     });
 
-    it(" Testing retrieval of all pet owners", () => {
+    it(" Testing retrieval of all pet owners", async () => {
         // Store adopters in memory rather than contract's storage
-        const adopters = adoption.getAdopters();
+        const adopters = await adoptionInstance.getAdopters();
         assert.equal(adopters[expectedPetId], expectedAdopter, "Owner of the expected pet should be this contract");
     });
     
